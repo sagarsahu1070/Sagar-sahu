8fcb8fcee928ed25b36e8fa3a55c0bf1095cf932
sagarsahu1070 <sagarsahu1070@gmail.com>
2026-02-16 11:33:19 +0530

    UC2 Complete

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+
+class OOPSBannerApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" ***** "+" "+" ***** "+" "+"****** "+" "+" ***** ");
+        System.out.println("*     *"+" "+"*     *"+" "+"*     *"+" "+"*     *");
+        System.out.println("*     *"+" "+"*     *"+" "+"*     *"+" "+"*      ");
+        System.out.println("*     *"+" "+"*     *"+" "+"****** "+" "+" ***** ");
+        System.out.println("*     *"+" "+"*     *"+" "+"*      "+" "+"      *");
+        System.out.println("*     *"+" "+"*     *"+" "+"*      "+" "+"      *");
+        System.out.println(" ***** "+" "+" ***** "+" "+"*      "+" "+"****** ");
+    }
+}
