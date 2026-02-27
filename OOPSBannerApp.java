69019b3f5feef0e7b8335e296bf5607272a56fa1
sagarsahu1070 <sagarsahu1070@gmail.com>
2026-02-27 23:45:27 +0530

    UC6 Done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,47 @@
 public class OOPSBannerApp {
+
+    static String[] o = new String[7];
+    static String[] p = new String[7];
+    static String[] s = new String[7];
+
+    public static void setOPattern() {
+        o[0] = " *** ";
+        o[1] = "*   *";
+        o[2] = "*   *";
+        o[3] = "*   *";
+        o[4] = "*   *";
+        o[5] = "*   *";
+        o[6] = " *** ";
+    }
+
+    public static void setPPattern() {
+        p[0] = "**** ";
+        p[1] = "*   *";
+        p[2] = "*   *";
+        p[3] = "**** ";
+        p[4] = "*    ";
+        p[5] = "*    ";
+        p[6] = "*    ";
+    }
+
+    public static void setSPattern() {
+        s[0] = " ****";
+        s[1] = "*    ";
+        s[2] = "*    ";
+        s[3] = " *** ";
+        s[4] = "    *";
+        s[5] = "    *";
+        s[6] = "**** ";
+    }
+
     public static void main(String[] args) {
-        String[] lines = new String[7];
 
-        lines[0] = String.join("  ", "  *****  ", "  *****  ", "  *****  ", "  *****  ");
-        lines[1] = String.join("  ", " **     **", " **     **", " **     **", " **     **");
-        lines[2] = String.join("  ", " **     **", " **     **", " **     **", " **      ");
-        lines[3] = String.join("  ", " **     **", " **     **", "  *****  ", "  *****  ");
-        lines[4] = String.join("  ", " **     **", " **     **", " **      ", "       **");
-        lines[5] = String.join("  ", " **     **", " **     **", " **      ", " **     **");
-        lines[6] = String.join("  ", "  *****  ", "  *****  ", " **      ", "  *****  ");
+        setOPattern();
+        setPPattern();
+        setSPattern();
 
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
         }
     }
 }
